7b079d031375d5e03918bd994501ce42201f2713
Diyorbek Ganjaev <diyorbekganjayev001@gmail.com>
2023-10-26 20:56:39 +0500

    Fayllar bilan ishlash mavzusi qo'shildi

---
+++ Fayllar/fayllar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 20:03:59 2023
+
+@author: user
+"""
+
+with open('pi_million_digits.txt') as file:
+    pi=file.read()
+    
+print(pi)
---
+++ xatolar_bilan_ishlash.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 19:19:23 2023
+
+@author: user
+"""
+
+# while True:
+#     yosh = input("Yoshingizni kiriting: ")
+#     if yosh.isdigit():
+#         if int(yosh)==0: 
+#             print("Nol yosh bo'lolmaysiz, qaytadan")
+#             continue
+#         else: 
+#             yosh=int(yosh)
+#             break        
+
+# print(f"Siz {2021-yosh} yilda tug'ilgansiz")
+
+# yosh = input("Yoshingizni kiriting: ")
+# try:
+#     yosh = int(yosh)    
+# except ValueError :
+#     print("Butun son kiritmadingiz")
+# else:
+#     print(f"Siz {2023-yosh} yilda tug'ilgansiz")
+
+# n = input("Butun son kiriting: ")
+# try:
+#     n = int(n)
+#     x=20/n
+# except ValueError: # agar foydalanuvchi butun son kiritmasa
+#     print("Butun son kiritmadingiz")
+# except ZeroDivisionError: # agar foydalanuvchi 0 kiritsa
+#     print("0 ga bo'lib bo'lmaydi")
+# else:
+#     print(f"x={x}")
